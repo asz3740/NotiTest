e3d4eae911e936f3e9ff2250b8d21bb4946ded79
Moon Jongmin <whdals0@naver.com>
2020-05-14 16:26:40 +0900

    1. FCM 푸시 테스트 소스 작업

--- app/src/main/java/com/roopre/notitest/MainActivity.java
+++ app/src/main/java/com/roopre/notitest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.roopre.notitest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -12,11 +13,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    String TAG = getClass().getSimpleName();
     Button noti_btn, release_btn;
 
     @Override
@@ -30,9 +39,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         noti_btn.setOnClickListener(this);
         release_btn.setOnClickListener(this);
 
-        createNotificationChannel();
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
 
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
 
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+//        createNotificationChannel();
     }
 
     private void createNotificationChannel() {
