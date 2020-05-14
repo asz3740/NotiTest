24b0b12a796b194e1cfa2dec6f104cc7df07ef29
Moon Jongmin <whdals0@naver.com>
2020-05-14 15:23:57 +0900

    Initial commit

---
+++ app/src/main/java/com/roopre/notitest/MainActivity.java
@@ -0,0 +1,99 @@
+package com.roopre.notitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button noti_btn, release_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noti_btn = findViewById(R.id.noti_btn);
+        release_btn = findViewById(R.id.release_btn);
+
+        noti_btn.setOnClickListener(this);
+        release_btn.setOnClickListener(this);
+
+        createNotificationChannel();
+
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "noti_name";
+            String description = "noti_description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.noti_btn:
+                ShowNotification();
+                break;
+            case R.id.release_btn:
+                break;
+        }
+    }
+
+    private void ShowNotification() {
+
+        String content = "알림내용입니다.\n알림내용\n알림내용\n알림내용\n알림내용\n알림내용\n알림내용\n알림내용\n알림내용\n알림내용\n알림내용";
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.dog1);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("알림 제목입니다.")
+                .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(largeIcon))
+//                .setStyle(new NotificationCompat.BigTextStyle()
+//                        .bigText(content))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            manager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        manager.notify(1,builder.build());
+
+
+
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//        // notificationId is a unique int for each notification that you must define
+//        notificationManager.notify(1, builder.build());
+
+    }
+}
